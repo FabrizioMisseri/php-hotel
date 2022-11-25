4893a0592129622c0ee7c010520543289266094a
FabrizioMisseri <misserifabrizio@gmail.com>
2022-11-25 21:17:46 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2,
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1,
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5,
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50,
+        ],
+
+    ];
+
+    $park_select = $_GET["park_select"] ?? "";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+
+<form action="index.php" method="GET">
+    <select name="park_select" id="">
+        <option value="true">con parcheggio</option>
+        <option value="false">senza parcheggio</option>
+    </select>
+    <button type="submit">BUTTONE</button>
+</form>
+    
+
+<table class="table">
+  <thead>
+
+    <tr>
+        <?php foreach ($hotels[0] as $title => $description) { ?>
+        
+        <th scope="col">
+            <?php echo $title ?>
+        </th>
+
+        <?php } ?>
+
+    </tr>
+
+  </thead>
+
+  <tbody>
+
+    <?php foreach ($hotels as $index) { ?>
+    <tr>
+        <?php foreach ($index as $title => $description) { ?>
+            <td>
+                <?php if($title === 'parking'){
+                    if($description === true){
+                        echo "c'è il parcheggio";
+                    } else {
+                        echo "non c'è il parcheggio";
+                    }
+                } else {
+                    echo $description;
+                }
+                ?>
+            </td>
+        <?php } ?>
+        
+    </tr>
+    <?php } ?>
+    
+  </tbody>
+</table>
+
+</body>
+</html>
