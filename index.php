c2512ca06680f9c15b56cdb4d8c957701072cc22
FabrizioMisseri <misserifabrizio@gmail.com>
2022-12-30 18:53:41 +0100

    hard refactoring

--- index.php
+++ index.php
@@ -1,164 +1,123 @@
 <?php
 
-    $hotels = [
+$hotels = [
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2,
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1,
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5,
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50,
-        ],
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
 
-    ];
-
-    $form_select = $_GET["form_select"] ?? "";
-    $form_vote = $_GET["form_vote"] ?? "";
-
+];
+$parking = $_GET["parking"] ?? "none";
+$vote = $_GET["vote"] ?? "0";
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
-<body>
 
-<!-- FORM -->
-<form action="index.php" method="GET">
-    <select name="form_select" id="">
-        <option value="all">tutti i tipi di hotel</option>
-        <option value="true">hotel con parcheggio</option>
-        <option value="false">hotel senza parcheggio</option>
-    </select>
-    <input type="number" name="form_vote" value="vote" placeholder="inserisci il voto da filtrare">
-    <button type="submit">BUTTONE</button>
-</form>
-<!-- / FORM -->
+<body style="margin-left: 1rem;">
+    <section style="margin-top: 1rem; margin-bottom: 2rem;">
+        <form action="index.php" method="GET">
 
-    
-<!-- TABELLA -->
-<table class="table">
+            <select name="parking" id="">
+                <option value="none">parcheggio indifferente</option>
+                <option value="1">con parcheggio</option>
+                <option value="0">senza parcheggio</option>
+            </select>
 
-  <thead>
+            <select name="vote" id="">
+                <option value="0">nessun voto</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
 
-    <tr>
-        <?php foreach ($hotels[0] as $title => $description) { ?>
-        
-        <th scope="col">
-            <?php echo $title ?>
-        </th>
+            <button type="submit">submittalo</button>
+        </form>
+    </section>
 
-        <?php } ?>
+    <section>
+        <table style="border-spacing: 0px;">
 
-    </tr>
+            <tr>
+                <?php foreach ($hotels[0] as $key => $value) { ?>
+                    <th style='border: 1px solid black;'>
+                        <?php echo "<strong> $key </strong>" ?>
+                    </th>
+                <?php } ?>
+            </tr>
 
-  </thead>
+            <?php
 
-  <tbody>
-
-        <!-- CON PARCHEGGIO -->
-        <?php foreach ($hotels as $index) { 
-            if($form_select === 'true'){
-                if ($index['parking'] === true) {
+            foreach ($hotels as $hotel) {
+                if ($parking == 'none' && intval($vote) <= $hotel['vote']) {
             ?>
-        <tr>
-            <?php foreach($index as $title => $description){ ?>
-            <td>
-                <?php 
-                    if ($title === 'parking') {
-                        echo "con parcheggio";
-                    } else {
-                        echo $description;
-
-                    }
+                    <tr>
+                        <?php foreach ($hotel as $key => $value) { ?>
+                            <td style='border: 1px solid black;'>
+                                <?php echo "$value" ?>
+                            </td>
+                        <?php } ?>
+                    </tr>
+                <?php } elseif ($hotel['parking'] == $parking && intval($vote) <= $hotel['vote']) {
                 ?>
-            </td>
-            <?php } ?>
-            
-            
-        </tr>
-        <!-- / CON PARCHEGGIO -->
+                    <tr>
+                        <?php foreach ($hotel as $key => $value) { ?>
+                            <td style='border: 1px solid black;'>
+                                <?php echo "$value" ?>
+                            </td>
+                        <?php } ?>
+                    </tr>
+            <?php }
+            } ?>
 
 
-
-        <!-- SENZA PARCHEGGIO -->
-        <?php }}elseif ($form_select === 'false') {
-                if ($index['parking'] === false) { ?>
-        <tr>
-            <?php foreach($index as $title => $description){ ?>
-                <td>
-                <?php 
-                    if ($title === 'parking') {
-                        echo "senza parcheggio";
-                    } else {
-                        echo $description;
-
-                    }
-                ?>
-            </td>
-            <?php } ?>
-        </tr>
-        <?php }} elseif($form_select === 'all'){?>
-        <!-- / SENZA PARCHEGGIO -->
+        </table>
+    </section>
 
 
-        <!-- ALL TYPE -->
-        <tr>
-            <?php foreach($index as $title => $description){ ?>
-                <td>
-                <?php 
-                    if($title === 'parking' && $description === true){
-                        echo "con parcheggio";
-                    } elseif ($title === 'parking' && $description === false) {
-                        echo "senza parcheggio";
-                    } else {
-                        echo $description;
-                    }
-                ?>
-                </td>
-            <?php } ?>
-        </tr>
-        <?php } } ?>
-        <!-- ALL TYPE -->
-    
-  </tbody>
-
-</table>
-<!-- / TABELLA -->
 
 </body>
+
 </html>
